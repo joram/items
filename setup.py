82161d8d1692ddb157fb9be4a0116e61269aee78
John Oram <john@oram.ca>
2021-07-11 21:51:25 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ setuptools.setup(
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/joram/mec_items",
+    url="https://github.com/joram/items",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
+    package_dir={"items": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
